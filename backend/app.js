d6e8556b9e17b97cdbe750e21fbb7d87df7ca8e3
Kamil <37276273+kamilkulik@users.noreply.github.com>
2020-01-18 13:29:11 +0100

    add a procfile

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 const cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var emailSender = require('./routes/mailer');
 var app = express();
 
@@ -22,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 app.use('/email', emailSender);
 
 // catch 404 and forward to error handler
